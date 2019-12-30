cf5e2bb44cbb23710a118d279045d6ba324ad58a
Vishal Ranjan <vishivish18@gmail.com>
2019-12-30 20:34:17 +0530

    changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,7 +9,10 @@ class Home extends React.Component {
       <div className="row text-center">
           <div className="col-8 offset-2">
             <img className="landing-logo" src="/logo.jpg"/>
-            <p className="landing-text">Learn! Play! Jam! Perform!</p>
+            <p className="landing-text">Learn!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Play!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Jam!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Perform!</p>
             <Link to="/courses">
             <Button outline color="danger">Start Your Musical Journey Now</Button>
             </Link>
