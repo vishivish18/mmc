6657724642198f4a0bd12dfaee26c193e31be584
Vishal Ranjan <vishivish18@gmail.com>
2019-12-30 19:37:09 +0530

    changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -21,13 +21,12 @@ class Contact extends React.Component {
         
       </div>
       <div className="row text-center">
-        <div className="col-6" style={{marginTop:120}}>
-        <div className="fb-page" data-href="https://www.facebook.com/MaestroMusicClub/" data-tabs="timeline" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/MaestroMusicClub/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/MaestroMusicClub/">Maestro Music Club - Music Academy</a></blockquote></div>
-        </div>
-
-      <div className="col-6">
+      <div className="col-12">
       <div className="mapouter"><div className="gmap_canvas"><iframe width="800" height="768" id="gmap_canvas" src="https://maps.google.com/maps?q=maestro%20music%20club&t=&z=12&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
       </div>
+      <div className="col-12" style={{marginTop:120}}>
+        <div className="fb-page" data-href="https://www.facebook.com/MaestroMusicClub/" data-tabs="timeline" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/MaestroMusicClub/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/MaestroMusicClub/">Maestro Music Club - Music Academy</a></blockquote></div>
+        </div>
       </div>
     </div>
     )
